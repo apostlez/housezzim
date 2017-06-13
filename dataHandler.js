06de50acdbd810a8cf953dacb7abfd5b7562a836
yunkwan.kim <yunkwan.kim@lge.com>
2017-06-13 18:59:42 +0900

    update sample to cache

---
+++ dataHandler.js
@@ -0,0 +1,17 @@
+
+(function () {
+	
+	var instance = {};
+	    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = instance;
+	
+	fs = require('fs')
+
+	fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(data);
+	});	
+}());
+
---
+++ launch.cmd
@@ -0,0 +1,3 @@
+@SET NW_DIR="%~dp0%\"
+cd %NW_DIR%
+@START /b C:\Users\admin\git\nwjs-sdk-v0.20.0-win-x64\nw.exe .
