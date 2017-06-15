83682e18fff03f84d0b1bcb81ef59dc778ebc8e7
yunkwan.kim <yunkwan.kim@lge.com>
2017-06-15 19:02:52 +0900

    add rent features

--- dataHandler.js
+++ dataHandler.js
@@ -3,13 +3,22 @@ var extention = '.xml';
 
 (function() {
 	var instance = {};
+	var key = 'PPiKlLLG9jBJcNvGn1NH66usYobBBPGCSuAXht3gOJ5hCC1tKqlCwpSQDKqfIciXI9TYmqUcP3bRg7pw56Te8w%3D%3D';
+	var url_list = {
+		house_trade : "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcRHTrade",
+		house_rent : "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcRHRent",
+		apart_trade : "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade",
+		apart_rent : ""
+	};
+	
+	
 	if (typeof module !== 'undefined' && module.exports) {
 		module.exports = instance;
 	}
 
-	instance.readCachedData = function(lawd_cd, year_month) {
+	instance.readCachedData = function(trade_type, lawd_cd, year_month) {
 		var fs = require('fs');
-		var filename = cache_loc + lawd_cd + '_' + year_month + extention;
+		var filename = cache_loc + trade_type + "\\" + lawd_cd + '_' + year_month + extention;
 		try {
 			var contents = fs.readFileSync(filename, 'utf8');
 			if (!contents) {
@@ -22,20 +31,33 @@ var extention = '.xml';
 		return contents;
 	};
 
-	instance.writeCache = function(lawd_cd, year_month, data) {
+	instance.writeCache = function(trade_type, lawd_cd, year_month, data) {
 		var fs = require('fs');
-		var filename = cache_loc + lawd_cd + '_' + year_month + extention;
-		var contents = fs.writeFile(filename, data, 'utf8', function(err) {
+		var path = cache_loc + trade_type;
+		var filename = path + "\\" + lawd_cd + '_' + year_month + extention;
+		
+		var exception_func = function(err) {
+					if(err) {
+						console.log(err);
+					}
+				};
+		if(fs.existsSync(path)) {
+			fs.writeFile(filename, data, 'utf8', exception_func);
+		} else {
+		fs.mkdir(path, function(err) {
 			if(err) {
 				console.log(err);
+			} else {
+				fs.writeFile(filename, data, 'utf8', exception_func);
 			}
 		});
+		}
+		
 	};
 	
-	instance.getDataOfYear = function (lawd_cd, deal_ymd, next) {
+	instance.getDataOfYear = function (trade_type, lawd_cd, deal_ymd, next) {
 		var xhr = new XMLHttpRequest();
-		var url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcRHTrade';
-		var key = 'PPiKlLLG9jBJcNvGn1NH66usYobBBPGCSuAXht3gOJ5hCC1tKqlCwpSQDKqfIciXI9TYmqUcP3bRg7pw56Te8w%3D%3D';
+		var url = url_list[trade_type];
 		var queryParams = '?serviceKey=' + key; /*Service Key*/
 		queryParams += '&' + encodeURIComponent('LAWD_CD') + '=' + encodeURIComponent(lawd_cd);
 		queryParams += '&' + encodeURIComponent('DEAL_YMD') + '=' + encodeURIComponent(deal_ymd);
@@ -43,12 +65,14 @@ var extention = '.xml';
 		xhr.onreadystatechange = function () {
 		    if (this.readyState == 4) {
 		    	strResponse = this.responseText;
-		    	instance.writeCache(lawd_cd, deal_ymd, strResponse);
+		    	instance.writeCache(trade_type, lawd_cd, deal_ymd, strResponse);
 				next();
 		    }
 		};
 		xhr.send('');
 	}
+	
+
 
 
 }());
--- housezzim.js
+++ housezzim.js
@@ -4,20 +4,34 @@ var searchResult = [];
 var search_addr = "";
 var search_detail_addr = "";
 
+var numbers_of_result = {};
+/*
+item scheme of summary_of_result
+{
+	<year> : {
+		<month>: <number>,
+		...
+	},
+	...
+}
+*/
+var summary_of_result = {};
+
 function getData(lawd_cd, addr, detail_addr) {
 	search_addr = addr;
 	search_detail_addr = detail_addr;
 	var yearStart = 2006;
 	var ymEnd = "201705";
 	var month = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+	var trade_type = "house_rent";
 	
 	var dataHandler = require("./dataHandler.js");
 	for(var i=yearStart;i<2017;i++) {
 		for(var j=0;j<12;j++) {
 			if(ymEnd == (i + month[j])) return; 
-			strResponse = dataHandler.readCachedData(lawd_cd, i + month[j]);
+			strResponse = dataHandler.readCachedData(trade_type, lawd_cd, i + month[j]);
 			if(!strResponse) {
-				dataHandler.getDataOfYear(lawd_cd, i + month[j], parseResponse);
+				dataHandler.getDataOfYear(trade_type, lawd_cd, i + month[j], parseResponse);
 			} else {
 				parseResponse();
 			}
@@ -51,6 +65,7 @@ var xml;
 
 function parseResponse() {
 	var addr = search_detail_addr;
+	var dong = search_addr;
 	//var addr = "387-2";
 	//var addr = "1170-3";
 	
@@ -62,13 +77,22 @@ function parseResponse() {
 	if(xmlDoc.getElementsByTagName("items")[0] != undefined ) {
 		itemArray = xmlDoc.getElementsByTagName("items")[0].childNodes;
 		for(var i = 0; i < itemArray.length; i++) {
-			if( itemArray[i].getElementsByTagName("지번")[0].innerHTML == addr) {
-				printItem(itemArray[i].childNodes);
-				itemToJson(itemArray[i].childNodes);
+			if( itemArray[i].getElementsByTagName("법정동")[0].innerHTML.trim() == dong) {
+				var year = itemArray[i].getElementsByTagName("년")[0].innerHTML.trim();
+				var month = itemArray[i].getElementsByTagName("월")[0].innerHTML.trim();
+				if(numbers_of_result[year] == undefined) numbers_of_result[year] = {};
+				if(numbers_of_result[year][month] == undefined) numbers_of_result[year][month] = 0;
+				numbers_of_result[year][month]++;
+				
+				if( itemArray[i].getElementsByTagName("지번")[0].innerHTML == addr) {
+					printItem(itemArray[i].childNodes);
+					itemToJson(itemArray[i].childNodes);
+				}
 			}
 		}
 	}
 	makeTable();
+	makeSummary();
 	//xml.getElementsByTagName("items")[0].childNodes[0].getElementsByTagName("지번")[0].innerHTML;
 	//document.getElementById("demo").innerHTML = itemArray[0];
 }
@@ -96,7 +120,8 @@ function makeTable() {
 	document.getElementById("table").innerHTML = "";
 	var strHeader = "";
 	var strOutput = "";
-	var keys = ["년", "월", "거래금액", "층", "전용면적", "대지권면적", "법정동"];
+	var keys = ["년", "월", "거래금액", "보증금액", "층", "전용면적", "대지권면적", "법정동"];
+	// "월세금액"
 	//alert(keys.toString());
 	strOutput += "<table>";
 	for(var j = 0; j < keys.length; j++) {
@@ -109,4 +134,22 @@ function makeTable() {
 	}
 	strOutput += "</table>";
 	document.getElementById("table").innerHTML = strOutput;
-}
+};
+
+function makeSummary() {
+	var strOutput = "";
+	// numbers_of_result
+	strOutput += "<table>";
+	
+	for(var j in Object.keys(numbers_of_result)) {
+		strOutput += "<tr>";
+		strOutput += "<td>" + j + "</td>";
+		for(var i in Object.keys(numbers_of_result[j])) {
+			strOutput += "<td>" + i + "</td>";
+			strOutput += "<td>" + numbers_of_result[j][i] + "</td>";
+		}
+		strOutput += "</tr>";
+	}
+	strOutput += "</table>";
+	document.getElementById("table").innerHTML = strOutput;
+};
